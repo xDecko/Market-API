6c4c2d8d424ba47c790b314708f654ee8fb96787
Decko <deckocol@gmail.com>
2023-08-25 13:01:13 -0500

    first

---
+++ src/main/java/com/decko/market/DeckoMarketApplication.java
@@ -0,0 +1,13 @@
+package com.decko.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeckoMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeckoMarketApplication.class, args);
+	}
+
+}
