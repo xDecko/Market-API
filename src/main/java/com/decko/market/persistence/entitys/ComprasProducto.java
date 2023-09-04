b16c949b47a0835e62370b6b6a3bab6224d4abe4
Decko <deckocol@gmail.com>
2023-09-04 01:03:02 -0500

    Creación de las Entitys

---
+++ src/main/java/com/decko/market/persistence/entitys/ComprasProducto.java
@@ -0,0 +1,46 @@
+package com.decko.market.persistence.entitys;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "compras_productos")
+public class ComprasProducto {
+    @EmbeddedId
+    private ComprasProductoPK id;
+
+    private Integer cantidad;
+    private Double total;
+    private Boolean estado;
+
+    public ComprasProductoPK getId() {
+        return id;
+    }
+
+    public void setId(ComprasProductoPK id) {
+        this.id = id;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Double getTotal() {
+        return total;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+}
