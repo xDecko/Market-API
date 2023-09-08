a49c582bb46923eff035063d0927f5afba697233
Decko <deckocol@gmail.com>
2023-09-08 02:18:47 -0500

    Agregados Endpoints & ResponseEntitys

--- src/main/java/com/decko/market/web/controler/ProductController.java
+++ src/main/java/com/decko/market/web/controler/ProductController.java
@@ -3,8 +3,10 @@ package com.decko.market.web.controler;
 import com.decko.market.domain.Product;
 import com.decko.market.domain.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,24 +17,37 @@ class ProductController {
     @Autowired
     private ProductService productService;
 
-    public List<Product> getAll(){
-        return productService.getAll();
+    @GetMapping("/all")
+    public ResponseEntity<List<Product>> getAll(){
+        return new ResponseEntity<>(productService.getAll(), HttpStatus.OK) ;
     }
 
-    public Optional<Product> getProduct(int productId) {
-        return  productService.getProduct(productId);
+    @GetMapping("/{id}")
+    public  ResponseEntity<Product> getProduct(@PathVariable("id") int productId) {
+        return  productService.getProduct(productId)
+                .map(product -> new ResponseEntity<>(product, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
-    public Optional<List<Product>> getByCategory(int categoryId){
-        return productService.getByCategory(categoryId);
+    @GetMapping("/category/{categoryId}")
+    public ResponseEntity<List<Product>> getByCategory(@PathVariable("categoryId") int categoryId){
+        return productService.getByCategory(categoryId)
+                .map(products -> new ResponseEntity<>(products,HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
-    public Product save(Product product){
-        return productService.save(product);
+    @PostMapping("/save")
+    public ResponseEntity<Product> save(@RequestBody Product product){
+        return new ResponseEntity<>(productService.save(product),HttpStatus.CREATED);
     }
 
-    public boolean delete(int productId){
-        return productService.delete(productId);
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity delete(@PathVariable("id") int productId){
+        if (productService.delete(productId)){
+            return new ResponseEntity(HttpStatus.OK);
+        } else {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
     }
 
 }
