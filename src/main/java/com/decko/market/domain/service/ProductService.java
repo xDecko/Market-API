7699bbb22f3660bea71376685a122989292a63b3
Decko <deckocol@gmail.com>
2023-09-11 03:18:28 -0500

    Conexion PostgreSQL

--- src/main/java/com/decko/market/domain/service/ProductService.java
+++ src/main/java/com/decko/market/domain/service/ProductService.java
@@ -36,4 +36,3 @@ public class ProductService {
         }).orElse(false);
     }
 }
-
--- src/main/java/com/decko/market/web/controler/ProductController.java
+++ src/main/java/com/decko/market/web/controler/ProductController.java
@@ -4,7 +4,6 @@ import com.decko.market.domain.Product;
 import com.decko.market.domain.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,43 +12,42 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/products")
-class ProductController {
+public class ProductController {
     @Autowired
     private ProductService productService;
 
     @GetMapping("/all")
-    public ResponseEntity<List<Product>> getAll(){
-        return new ResponseEntity<>(productService.getAll(), HttpStatus.OK) ;
+    public ResponseEntity<List<Product>> getAll() {
+        return new ResponseEntity<>(productService.getAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public  ResponseEntity<Product> getProduct(@PathVariable("id") int productId) {
-        return  productService.getProduct(productId)
+    public ResponseEntity<Product> getProduct(@PathVariable("id") int productId) {
+        return productService.getProduct(productId)
                 .map(product -> new ResponseEntity<>(product, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @GetMapping("/category/{categoryId}")
-    public ResponseEntity<List<Product>> getByCategory(@PathVariable("categoryId") int categoryId){
+    public ResponseEntity<List<Product>> getByCategory(@PathVariable("categoryId") int categoryId) {
         return productService.getByCategory(categoryId)
-                .map(products -> new ResponseEntity<>(products,HttpStatus.OK))
+                .map(products -> new ResponseEntity<>(products, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @PostMapping("/save")
-    public ResponseEntity<Product> save(@RequestBody Product product){
-        return new ResponseEntity<>(productService.save(product),HttpStatus.CREATED);
+    public ResponseEntity<Product> save(@RequestBody Product product) {
+        return new ResponseEntity<>(productService.save(product), HttpStatus.CREATED);
     }
 
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity delete(@PathVariable("id") int productId){
-        if (productService.delete(productId)){
+    public ResponseEntity delete(@PathVariable("id") int productId) {
+        if (productService.delete(productId)) {
             return new ResponseEntity(HttpStatus.OK);
         } else {
             return new ResponseEntity(HttpStatus.NOT_FOUND);
         }
     }
-
 }
 
 
